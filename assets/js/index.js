f49b969ac8ef77021333a6529abc46b06c6ab8a5
jon <jonwhewaycode@outlook.com>
2021-01-30 19:50:20 +0000

    chaned the logo transistion to css and added the consent cookie

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
 
-    $("#company-name").animate({left: $("#company-name").parent().width() / 2 - $("#company-name").width() / 2}, 1000, 'linear');
-    $("#company-tec").animate({right: $("#company-tec").parent().width() / 2 - $("#company-tec").width() / 2}, 1000, 'linear');
+    $("#company-name").delay(800).fadeIn(2000);
+    $("#company-tec").delay(1000).fadeIn(2000);
+    // $("#company-name").animate({left: $("#company-name").parent().width() / 2 - $("#company-name").width() / 2}, 1000, 'linear');
+    // $("#company-tec").animate({right: $("#company-tec").parent().width() / 2 - $("#company-tec").width() / 2}, 1000, 'linear');
 
 })
 
@@ -10,4 +12,16 @@ function scrolltoview(itemID){
     let item = document.getElementById(itemID);
     console.log(item);
     item.scrollIntoView({behavior: 'smooth'});
+}
+
+// This Function looks at the screen size and changes the css on the Css on the navigation bars
+if ($(window).width() >= 769){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.navbar').css({"background-color":"#1C2331"});
+        } 
+        else{
+            $('.navbar').css({"background-color":"rgba(0,0,0,.2)"});
+        }
+    });
 }
